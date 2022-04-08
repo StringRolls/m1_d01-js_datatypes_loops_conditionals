c91504bf0fb3b949af56a28481ec2d5a6e01b82c
Marco Santonastasi <84954908+marco-ih-bcn@users.noreply.github.com>
2022-04-08 12:53:55 +0200

    updated solutions

--- exercises/03-exe-js_conditionals.js
+++ exercises/03-exe-js_conditionals.js
@@ -9,3 +9,37 @@
 
 const language = "English"
 // const name = ... <- Complete here and keep wriritng your code below
+
+// 1A: if...else - simple solution
+
+if (language === "spanish") console.log(`Hola, Martin`);
+else if(language === "french") console.log(`Salut, Martin`);
+else if (language === "english") console.log(`Hello, Martin`);
+else console.log("I am not sure what are you trying to do, sorry! 🍺");
+
+// 1B: if...else - more dynamic solution, asking for input from user using prompt()
+const language = prompt("Please enter the language - (options: spanish, french, english): ");
+const name = prompt("Please enter your name:");
+
+if (language === "spanish") { // if we will have more than one thing to console or return we have to use curly braces
+  console.log(`Hola, ${name}`);
+  console.log("Something else");
+}
+else if(language === "french") console.log(`Salut, ${name}`);
+else if (language === "english") console.log(`Hello, ${name}`);
+else console.log("I am not sure what are you trying to do, sorry! 🍺");
+
+// 2: switch
+switch(language){
+  case "spanish":
+    console.log(`Hola, ${name}`);
+    break;
+  case "french":
+    console.log(`Salut, ${name}`);
+    break;
+  case "english":
+    console.log(`Hello, ${name}`);
+    break;
+  default:
+    console.log("I am not sure what are you trying to do, sorry! 🍺");
+}
