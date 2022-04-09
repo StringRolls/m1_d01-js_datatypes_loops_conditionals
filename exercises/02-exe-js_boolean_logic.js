278698c1d6374db8bb73c9301a89f69b85b808e0
Marco Santonastasi <84954908+marco-ih-bcn@users.noreply.github.com>
2022-04-09 11:58:01 +0200

    clean exercises

--- exercises/02-exe-js_boolean_logic.js
+++ exercises/02-exe-js_boolean_logic.js
@@ -38,9 +38,9 @@ if(true || false){
 }
 
 // Write down what will these statements return:
-console.log(true && false); // ==> false
-console.log(11 % 3 === 2); // ==> true
-console.log(false || true); // ==> true
-console.log(!true || false); // ==> false
-console.log(17 == '17'); // ==> true
-console.log(123 === '123'); // ==> false
+console.log(true && false); // ==> ??
+console.log(11 % 3 === 2); // ==> ??
+console.log(false || true); // ==> ??
+console.log(!true || false); // ==> ??
+console.log(17 == '17'); // ==> ??
+console.log(123 === '123'); // ==> ??
